6c7459dcbc4e38d4a854a64856a0ebb09fefeaf9
bouchyelidrissi <elidrissi10bouchra@gmail.com>
2023-08-02 16:24:18 -0700

    ALX HIGH LEVEL

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        rst = fct(*args)
+        return (rst)
+    except (IndexError, ZeroDivisionError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
